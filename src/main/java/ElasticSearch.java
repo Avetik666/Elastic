9579e6cada96f96af832759593f052370cc050d9
armanmac <arman.piloyan@picsart.com>
2017-03-02 17:21:22 +0400

    proper working queries for Crash count and Crash count for unique device id-s

--- src/main/java/ElasticSearch.java
+++ src/main/java/ElasticSearch.java
@@ -1,3 +1,5 @@
+
+
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.common.settings.Settings;
@@ -5,59 +7,23 @@ import org.elasticsearch.common.transport.InetSocketTransportAddress;
 import org.elasticsearch.common.transport.TransportAddress;
 import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.aggregations.AggregationBuilders;
+import org.elasticsearch.search.aggregations.metrics.cardinality.Cardinality;
+import org.elasticsearch.search.aggregations.metrics.cardinality.CardinalityAggregationBuilder;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.elasticsearch.transport.client.PreBuiltTransportClient;
 
 import java.io.IOException;
 import java.net.InetAddress;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
 
 import static org.elasticsearch.index.query.QueryBuilders.matchAllQuery;
 import static org.elasticsearch.index.query.QueryBuilders.rangeQuery;
+
 /**
  * Created by armanmac on 2/16/17.
  */
 public class ElasticSearch {
-    public static long[] time(String from, String to) {
-        long date[] = new long[2];
-        SimpleDateFormat df = new SimpleDateFormat("MMM dd yyyy HH:mm:ss.SSS zzz");
-        Date datefrom = null;
-        Date dateto = null;
-        if (to == null) {
-            Calendar calobj = Calendar.getInstance();
-            try {
-                String current = df.format(calobj.getTime());
-                dateto = df.parse(current);
-            } catch (ParseException e) {
-                e.printStackTrace();
-            }
-        } else {
-            try {
-                dateto = df.parse(to);
-            } catch (ParseException e) {
-                e.printStackTrace();
-            }
-        }
-        try {
-            datefrom = df.parse(from);
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        long epochfrom = datefrom.getTime();
-        long epochto = dateto.getTime();
-        date[0] = epochfrom;
-        date[1] = epochto;
-        return date;
-
-    }
     public static void main(String[] args) throws IOException {
-        long[] time = time("Mar 01 2017 15:53:00.000 UTC", null);
-        //System.out.println(time[0]+ "\n" +  time[1]);
-
-
         Settings settings = Settings.builder()
                 .put("cluster.name", "PicsArtCluster")
                 .put("node.name", "ny-log1").build();
@@ -65,21 +31,64 @@ public class ElasticSearch {
         Client client = new PreBuiltTransportClient(settings).
                 addTransportAddress(address);
         QueryBuilder qb = matchAllQuery();
+
+
         //query: Crash count
         QueryBuilder test_query = QueryBuilders
                 .boolQuery()
                 .must(QueryBuilders.queryStringQuery("*").analyzeWildcard(true))
-                .must(rangeQuery("@timestamp").gte(Long.toString(time[0])).lte(Long.toString(time[1])).format("epoch_millis"));
+                .must(rangeQuery("@timestamp").gte("1488449959307" ).lte("1488450859307").format("epoch_millis"));
+
         SearchSourceBuilder searchSourceBuilder1 = new SearchSourceBuilder();
         searchSourceBuilder1.query(test_query);
         searchSourceBuilder1.size(0);
+
+
         //sending search query for Crash count
         SearchResponse response1 = client.prepareSearch("crashlytics-2017.03")
                 .setFetchSource(true)
                 .setQuery(searchSourceBuilder1.query())
                 .get();
+
         System.out.println(response1.getHits().totalHits());
-        //query:
+
+
+
+
+        //sending search query for unique device ID Crash count
+        CardinalityAggregationBuilder aggregation = AggregationBuilders
+                .cardinality("agg")
+                .field("device_id");
+
+
+        SearchResponse response2 = client.prepareSearch("crashlytics-2017.03")
+                .setFetchSource(true)
+                .setQuery(searchSourceBuilder1.query())
+                .addAggregation(aggregation)
+                .get();
+
+        Cardinality agg = response2.getAggregations().get("agg");
+        System.out.println(agg.getValue());
+
+
+
+
+
+
+        client.close();
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     }
 }
